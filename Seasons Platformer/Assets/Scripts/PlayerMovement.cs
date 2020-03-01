fa7ebb42bae61788919adeb3cd5f5b72c07e6c44
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2020-03-01 18:10:39 +0000

    Added left/right and jump
    Still need to add parameters to jump, stop multipled jumping, changing direction mid jump.

--- Seasons Platformer/Assets/Scripts/PlayerMovement.cs
+++ Seasons Platformer/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float movementSpeed;
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.Translate(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+        }
     }
 }
