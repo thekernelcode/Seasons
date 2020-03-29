c862baae2d0b71fec5b6f8c2eba4509935a98c13
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2020-03-29 19:47:12 +0100

    Added single jump controller

--- Seasons Platformer/Assets/Scripts/PlayerMovement.cs
+++ Seasons Platformer/Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,14 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public float movementSpeed;
+
     public Vector3 jump;
     public float jumpForce = 2.0f;
+
+    bool onGround = true;
+
+    public float landedCounterLength = 0.5f;
+    public float landedCounter;
 
     Rigidbody rb;
 
@@ -16,17 +22,42 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 2.0f, 0.0f);
+        float landedCounter = landedCounterLength;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.Translate(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0, 0);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (onGround == true)
         {
+            this.transform.Translate(Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime, 0, 0);
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space) && onGround == true && landedCounter <= 0)
+        {
+            onGround = false;
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            Debug.Log("Jumped");
+            landedCounter = landedCounterLength;
+        }
+
+        if (landedCounter > 0)
+        {
+            landedCounter -= Time.deltaTime;
+        }
+
+    }
+
+    void OnCollisionStay()
+    {
+        if (landedCounter <= 0f)
+        {
+            onGround = true;
+            Debug.Log("Landed");
+            landedCounter = landedCounterLength;
         }
     }
+
+
+
 }
