457f19736f299c84aaba07e06ada730a8781b867
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2020-03-30 15:16:46 +0100

    Added change to Y velocity if 'banging head'

--- Seasons Platformer/Assets/Scripts/PlayerMovement.cs
+++ Seasons Platformer/Assets/Scripts/PlayerMovement.cs
@@ -14,6 +14,9 @@ public class PlayerMovement : MonoBehaviour
     private Vector3 moveDirection;
     public float gravityScale;
 
+    public float currentYValue;
+    public float lastYValue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,13 +34,25 @@ public class PlayerMovement : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 moveDirection.y = jumpForce;
+                
+            }
+        }
+
+        //lastYValue = transform.position.y;
+
+        if (theCharController.isGrounded == false)
+        {
+            currentYValue = transform.position.y;
+            if (currentYValue == lastYValue)
+            {
+                moveDirection.y = 0;
             }
         }
        
-        //TODO - When 'bang head' on platform, change y movement to fall to ground.
-
         moveDirection.y = moveDirection.y + (Physics.gravity.y * gravityScale * Time.deltaTime);
         theCharController.Move(moveDirection * Time.deltaTime);
+
+        lastYValue = currentYValue;
 
     }
 
